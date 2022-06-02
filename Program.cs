27b2df77d90834268b60a0f90151b3732c1d89fe
John Tuck <j.tuck1336@gmail.com>
2022-06-01 22:16:33 -0400

    exercise 2 for methods. almost had the challenge hehe

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Method_Exercise_2
+{
+    internal class Program
+    {
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+    static void Main(string[] args)
+        {
+            Multiply(10, 2);
+        }
+        
+
+        
+
+       
+    }
+
+}
